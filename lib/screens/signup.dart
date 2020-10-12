b401dfa6fee58d33eba5de00da9a8a3441994575
Adrian De León <adriandleon@gmail.com>
2020-10-11 23:25:18 -0300

    Add SignUp form

---
+++ lib/screens/signup.dart
@@ -0,0 +1,180 @@
+import 'package:flutter/material.dart';
+
+class SignUp extends StatefulWidget {
+  @override
+  _SignUpState createState() => _SignUpState();
+}
+
+final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+bool passwordVisible = false;
+
+bool validateEmail(String value) {
+  Pattern pattern =
+      r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+  RegExp regex = new RegExp(pattern);
+  return (!regex.hasMatch(value)) ? false : true;
+}
+
+class _SignUpState extends State<SignUp> {
+  void validation() {
+    final FormState _form = _formKey.currentState;
+    if (_form.validate()) {
+      print('Yes');
+    } else {
+      print('No');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: SafeArea(
+        child: Form(
+          key: _formKey,
+          child: Container(
+            child: Column(
+              children: <Widget>[
+                Container(
+                  height: 220,
+                  width: double.infinity,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: <Widget>[
+                      Text(
+                        'Register',
+                        style: TextStyle(
+                          fontSize: 50,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  height: 400,
+                  margin: EdgeInsets.symmetric(horizontal: 16),
+                  width: double.infinity,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      TextFormField(
+                        validator: (value) {
+                          if (value == "") {
+                            return "Please fill UserName";
+                          } else if (value.length < 6) {
+                            return "UserName is too short";
+                          }
+                          return "";
+                        },
+                        decoration: InputDecoration(
+                          hintText: 'UserName',
+                          hintStyle: TextStyle(color: Colors.black),
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                      TextFormField(
+                        validator: (value) {
+                          if (value == "") {
+                            return "Please fill email";
+                          } else if (!validateEmail(value)) {
+                            return "Email is invalid";
+                          } else {
+                            return "";
+                          }
+                        },
+                        decoration: InputDecoration(
+                          hintText: 'Email',
+                          hintStyle: TextStyle(color: Colors.black),
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                      TextFormField(
+                        obscureText: passwordVisible,
+                        validator: (value) {
+                          if (value == "") {
+                            return "Please fill password";
+                          } else if (value.length < 6) {
+                            return "Password is too short";
+                          } else {
+                            return "";
+                          }
+                        },
+                        decoration: InputDecoration(
+                          hintText: 'Password',
+                          suffixIcon: GestureDetector(
+                            onTap: () {
+                              setState(() {
+                                passwordVisible = !passwordVisible;
+                              });
+                              FocusScope.of(context).unfocus();
+                            },
+                            child: Icon(
+                              passwordVisible
+                                  ? Icons.visibility
+                                  : Icons.visibility_off,
+                              color: Colors.black,
+                            ),
+                          ),
+                          hintStyle: TextStyle(color: Colors.black),
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                      TextFormField(
+                        validator: (value) {
+                          if (value.isEmpty) {
+                            return "Please fill phone number";
+                          } else if (value.length < 11) {
+                            return "Phone number must be 11";
+                          } else {
+                            return "";
+                          }
+                        },
+                        decoration: InputDecoration(
+                          hintText: 'Phone Number',
+                          hintStyle: TextStyle(color: Colors.black),
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                      Container(
+                        height: 45,
+                        width: double.infinity,
+                        child: RaisedButton(
+                            child: Text('Register'),
+                            color: Colors.blueGrey[400],
+                            onPressed: () {
+                              validation();
+                            }),
+                      ),
+                      Row(
+                        children: [
+                          Text('I have already an account!'),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          GestureDetector(
+                            child: Text(
+                              'Login',
+                              style: TextStyle(
+                                  color: Colors.cyan,
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
